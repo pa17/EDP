e5cc1a17246799bb5b5501aa7d08fa5c89502683
pa17 <paolofalco@gmx.ch>
2017-05-16 15:43:54 +0100

    Update ADCTestIter2.py

--- ADCTestIter2.py
+++ ADCTestIter2.py
@@ -4,6 +4,7 @@ import time
 import Adafruit_ADS1x15
 # Create an ADS1115 ADC (16-bit) instance.
 adc = Adafruit_ADS1x15.ADS1115()
+# Connect TMP to A0 and RV to A1
 
 # Gain for ADC
 GAIN = 1
@@ -15,33 +16,45 @@ RV_Wind_Volts = 0.0
 lastMillis = 0
 TempCtimes100 = 0
 zeroWind_ADunits = 0.0
-zeroWind_volts = 0.0
+zeroWind_V olts = 0.0
 WindSpeed_MPH = 0.0
 WindSpeed_MetresPerSecond = 0.0
 VolFlowRate = 0.0
 
+# Read all the ADC channel values in a list.
+values = [0]*4
+scaledvalues = [0]*4
 
-print('Reading ADS1x15 values, press Ctrl-C to quit...')
+print('Reading ADS1x15 values from Wind sensor, press Ctrl-C to quit...')
 # Print nice channel column headers.
 print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
 print('-' * 37)
 # Main loop.
 while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*4
-    scaledvalues = [0]*4
+
     for i in range(4):
         # Read the specified ADC channel using the previously set gain value.
         values[i] = adc.read_adc(i, gain=GAIN)
-        scaledvalues[i] = values[i]*0.01561
-        # Note you can also pass in an optional data_rate parameter that controls
-        # the ADC conversion time (in samples/second). Each chip has a different
-        # set of allowed data rate values, see datasheet Table 9 config register
-        # DR bit values.
-        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
-        # Each value will be a 12 or 16 bit signed integer value depending on the
-        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+        scaledvalues[i] = values[i]*0.01561 # Values scaled to 10 bit so that
+        # Temp reading
+        TMP_Therm_ADunits = scaledvalues[0]
+        # RV reading
+        RV_Wind_ADunits = scaledvalues[1]
+        RV_Wind_Volts = (RV_Wind_ADunits * 0.0048828125)
+        # Calculate temperature
+        TempCtimes100 = (0.005*TMP_Therm_ADunits*TMP_Therm_ADunits) - 16.862*TMP_Therm_ADunits + 9075.4
+        # Calculate zero wind
+        zeroWind_ADunits = -0.0006*TMP_Therm_ADunits*TMP_Therm_ADunits + 1.0727*TMP_Therm_ADunits + 47.172
+        # Zero wind adjustment
+        zeroWind_Volts = (zeroWind_ADunits * 0.0048828125) - zeroWindAdjustment
+        
+        # Wind speed in MPH
+        WindSpeed_MPH = ((RV_Wind_Volts - zeroWind_Volts)/0.2300)**2.7265
+        WindSpeed_MetresPerSecond = WindSpeedMPH * 0.44704
+        VolFlowRate = 6.931 * WindSpeed_MetresPerSecond
+        
     # Print the ADC values.
     print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*scaledvalues))
+    print VolFlowRate
     # Pause for half a second.
     time.sleep(0.5)
