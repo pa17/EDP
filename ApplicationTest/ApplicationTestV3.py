b4400243b14a4650854d8d761d61f7b25d14bb35
pa17 <paolofalco@gmx.ch>
2017-06-05 15:26:35 +0100

    Update ApplicationTestV3.py

--- ApplicationTest/ApplicationTestV3.py
+++ ApplicationTest/ApplicationTestV3.py
@@ -144,7 +144,7 @@ class CustomWidget(QtGui.QWidget):
     def ClassUpdatePlot(self):
         global ButtonFlag
         updatePlot()
-        self.TVDisplay.setText(Breath[-1])
+        self.ui.TVDisplay.setText(Breath[-1])
       
         if ButtonFlag == "WS":
             self.ui.plotWidget.plot(TimeList, WSList, clear=True, title="Breath speed vs. time")
